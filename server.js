69c0c14e29deb09df7723bf20b1032fee0ecbca7
Nguyen Trung Chinh <nguyentrungchinh@Nguyens-MacBook-Pro.local>
2023-10-22 00:21:00 +0700

    sixth commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 
 const buildPath = path.join(__dirname, 'build')
